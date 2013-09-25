aff090792f175e48e8b47c299c39a82f2e05f944
Samuel Thompson <ipqb@iPQBs-MacBook-Pro.local>
2013-09-25 11:14:31 -0700

    Input descriptions of initial functions.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # main.py
 
+
+def readFasta():
+"""Takes a location/filename as a string input. Opens the file and converts
+the alignment file to either a matrix of the characters, or a list of lists.
+Returns the fasta in python architecture."""
+
+
+def processFasta():
+"""Takes the read Fasta from readFast. Calls a scoring function for each line
+of the alignment. Returns the consensus sequence as a list and a corresponding
+list of the conservation values."""
