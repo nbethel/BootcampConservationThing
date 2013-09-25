d56b30b5c6860f5a8002fd05f35c57722ad18a12
Kevin Hartman <khartman@udp085277uds.ucsf.edu>
2013-09-25 13:24:07 -0700

    progress on scoring functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 import itertools
+import pylab
 
 
 def readFasta():
@@ -16,33 +17,45 @@ def processFasta():
 
 
 def VisualizeData(consensus_residues, conservation_scores):
+	# 
+	print 'poop'
 
-def makeScoreDict(filepath):
-	file = open(filepath, 'r')
+def make_score_dict(filepath):
 	
+	file = open(filepath, 'r')
 	dict = {}
-
+	
 	for i in file:
 		line = i.split()
 		dict[(line[0], line[1])] = float(line[2])
 	
 	return dict
 
-def scoreColumn(column_list, scoreDict):
+def score_column(column_list, score_dict):
 	# 
 	residue_pairs = itertools.combinations(column_list, 2)
-	score = 0
+	score = 0.0
 	
-	for i in residue_pairs:
-		new_score = scoreDict[i]
+
+	for i,j in residue_pairs:
+		new_score = score_dict[tuple(sorted([i, j]))]
 		score = score + new_score
 		
-	return score
+	n_terms = len(column_list) * (len(column_list) - 1)
+	normalized_score = score / n_terms
+	return normalized_score
+
+
 
 # 
-# scoreDict = makeScoreDict('blosum62.dat')
+# score_dict = make_score_dict('blosum62.dat')
 # 
+# residues = ['A', 'A', 'A', 'C']
+# print residues * 4
+# my_list = []
 # 
-# my_column = ['A', 'A', 'A', ]
-# print scoreColumn (my_column, scoreDict)
-
+# for i in range(20):
+# 	my_list.append(score_column(residues * (i+1), score_dict))
+# 
+# pylab.plot(my_list)
+# pylab.show()
