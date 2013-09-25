55acee364545cacec705156ab96c44b1bdabb42f
Kevin Hartman <khartman@udp085277uds.ucsf.edu>
2013-09-25 11:55:16 -0700

    Functional scoring functions

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # main.py
+import itertools
+
 
 def readFasta():
-"""Takes a location/filename as a string input. Opens the file and converts
-the alignment file to either a matrix of the characters, or a list of lists.
-Returns the fasta in python architecture."""
+	"""Takes a location/filename as a string input. Opens the file and converts
+	the alignment file to either a matrix of the characters, or a list of lists.
+	Returns the fasta in python architecture."""
 
 
 def processFasta():
-"""Takes the read Fasta from readFast. Calls a scoring function for each line
-of the alignment. Returns the consensus sequence as a list and a corresponding
-list of the conservation values."""
+	"""Takes the read Fasta from readFast. Calls a scoring function for each line
+	of the alignment. Returns the consensus sequence as a list and a corresponding
+	list of the conservation values."""
 
 
 def makeScoreDict(filepath):
@@ -19,20 +21,26 @@ def makeScoreDict(filepath):
 
 	for i in file:
 		line = i.split()
-		dict[(line[0], line[1])] = line[2]
+		dict[(line[0], line[1])] = float(line[2])
 	
 	return dict
-	
-def scoreColumn(column_list):
+
+def scoreColumn(column_list, scoreDict):
 	# 
-	
-	
-	
-	
+	residue_pairs = itertools.combinations(column_list, 2)
 	score = 0
+	
+	for i in residue_pairs:
+		new_score = scoreDict[i]
+		score = score + new_score
+		
 	return score
 
-scoreDict = makeScoreDict('blosum62.dat')
-print scoreDict[('A','A')]
+# 
+# scoreDict = makeScoreDict('blosum62.dat')
+# 
+# 
+# my_column = ['A', 'A', 'A', ]
+# print scoreColumn (my_column, scoreDict)
 
 
