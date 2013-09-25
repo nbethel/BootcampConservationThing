db12f1ec71f232a5698c45da9f94f703ac6f9529
Kevin Hartman <khartman@udp085277uds.ucsf.edu>
2013-09-25 10:45:49 -0700

    Changed to method

--- codons.py
+++ codons.py
@@ -1,10 +1,12 @@
-import math
-import numpy
+def makeCodonLib():
 
-dict={}
-file=open("../assignment/dna-codons.dat","r")
-for i in file:
-  line=i.split()
-  dict[line[0]]=line[1]
+	import math
+	import numpy
 
-print dict['TTT']
+	dict={}
+	file=open("../assignment/dna-codons.dat","r")
+	for i in file:
+	  line=i.split()
+	  dict[line[0]]=line[1]
+
+	return dict
