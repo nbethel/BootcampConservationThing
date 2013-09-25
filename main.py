fc9d35b51777db4b09fa94f6dd776e03ddbaa09c
Kevin Hartman <khartman@udp085277uds.ucsf.edu>
2013-09-25 11:18:15 -0700

    Added scoring functions outline

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 # main.py
 
+
+def makeScoreDict(filepath):
+	file = open(filepath, 'r')
+	
+	dict = {}
+
+	for i in file:
+		line = i.split()
+		dict[(line[0], line[1])] = line[2]
+	
+	return dict
+	
+def scoreColumn(column_list):
+	# 
+	
+	
+	
+	
+	score = 0
+	return score
+
+scoreDict = makeScoreDict('blosum62.dat')
+print scoreDict[('A','A')]
